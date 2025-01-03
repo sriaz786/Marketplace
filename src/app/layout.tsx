5f99d81d2b9ac763d5d5d6f11a82f41bc9fc4cf2
sriazs786 <sriazs2013@gmail.com>
2025-01-02 22:43:11 -0800

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import { Header } from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header />
+      <Hero/>
+
         {children}
+        <Footer/>
       </body>
     </html>
   );
